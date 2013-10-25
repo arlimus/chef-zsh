e131a1666942bcbd4b580746010d96ee701cf4c7
Dominik Richter <dominik.richter@gmail.com>
2013-10-25 03:39:10 +0200

    bugfix: activate zsh configuration via wildcard instead of path
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,7 +34,7 @@ end
 package "zsh"
 
 # add configuration for all users
-search("users","shell:/usr/bin/zsh").each do |c|
+search("users","shell:*zsh").each do |c|
   Chef::Log.info "gitconfig: setting shell to zsh foruser #{c['id']}"
   
   # presets
